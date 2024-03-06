45f8189a8c83d07850f0a61f7788d4c3010a8a7a
Muhammad Uzair Shahid Syed <msyed46@gatech.edu>
2024-03-05 18:00:13 -0700

    Updated with comments

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -32,13 +32,12 @@ class CalculatorApp extends JFrame implements ActionListener {
     private JTextField calculatedFieldText; // Display the calculated formula
     private JTextField invisibleField;
     private boolean starting_or_clear = true; // For removing 0 when calculator starts or is cleared
-    private char operationEnvoked = ' ';
-    private char prevoperationEnvoked = ' ';
+    private char operationEnvoked = ' '; // Track the operation envoked "+ - / x"
+    private char prevoperationEnvoked = ' '; // Track the previous operation envoked for history
     private boolean requestedOperation = false;
     private float pnum1 = 0; // Previous calculated numbers. Start from 0
     private float pnum2 = 0; // Previous calculated numbers. Start from 0
-    private float ans = 0;
-    private int counter = 0;
+    private float ans = 0; // Update for answers after calculation is performed
 
     /** Constructor - Calculator App. Takes no arguments */
     public CalculatorApp() {
@@ -53,7 +52,7 @@ class CalculatorApp extends JFrame implements ActionListener {
         Color calculatedFieldFontColor = new Color(130, 130, 130);
         Font displayFieldFont = new Font ("Arial", Font.PLAIN, 40);
 
-        // Enter Invisible Field
+        // Enter Invisible Field - This is to give some top border for the app
         invisibleField =new JTextField();
         invisibleField.setPreferredSize(new Dimension(400, 20));
         invisibleField.setEditable(false);
@@ -94,12 +93,10 @@ class CalculatorApp extends JFrame implements ActionListener {
         // Create BoxLayout for vertical expansion
         JPanel displayFieldPanel = new JPanel();
         displayFieldPanel.setLayout(new BoxLayout(displayFieldPanel, BoxLayout.Y_AXIS));
-
         displayFieldPanel.add(Box.createVerticalGlue());
         displayFieldPanel.add(invisibleField);
         displayFieldPanel.add(calculatedFieldText);
         displayFieldPanel.add(displayFieldText);
-
         add(displayFieldPanel, BorderLayout.NORTH);
 
         // Set Button Panel
@@ -116,14 +113,11 @@ class CalculatorApp extends JFrame implements ActionListener {
         opPanel.setBackground(calculatorBackground);
         initOpBtns(opPanel);
 
-
-
         // Enable Keypad and KeyBoard entry
         displayFieldText.addKeyListener(new KeyAdapter() {
-
             @Override
             public void keyTyped(KeyEvent e) {
-
+                // Ignore
             }
 
             @Override
@@ -133,6 +127,7 @@ class CalculatorApp extends JFrame implements ActionListener {
 
             @Override
             public void keyReleased(KeyEvent e) {
+                // Ignore
             }
         });
         // Add components to main layout
@@ -148,7 +143,6 @@ class CalculatorApp extends JFrame implements ActionListener {
     * */
     public boolean decimalExists() {
         boolean decimalExists = false;
-
         String currentText = displayFieldText.getText();
         for (int i = 0; i < currentText.length(); i++) {
             if (currentText.charAt(i) == '.') {
@@ -159,6 +153,11 @@ class CalculatorApp extends JFrame implements ActionListener {
         return decimalExists;
     }
 
+    /**
+     * This method is used to detect back space entered and removes a character from
+     * the input
+     * @params Returns an updated String value
+     **/
     public String backSpaceEntered() {
         String currentText = displayFieldText.getText();
         return currentText.substring(0, currentText.length() - 1);
@@ -167,34 +166,44 @@ class CalculatorApp extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
+        // Grabs the Button object and checks the text on the button for operations
         if (source instanceof JButton){
             JButton clickedButton = (JButton) source;
             String buttonOperation = clickedButton.getText();
+
+            // If button is a number, then enter a number in the display field
             if (buttonOperation.matches("[0-9]")) {
                 int number = Integer.parseInt(buttonOperation);
                 enterNumber(number);
             }
+            // If button is Clear (CLR), then clear the display field
             else if (buttonOperation.equals("CLR")) {
                 displayFieldText.setText("0");
                 calculatedFieldText.setText("");
                 displayFieldText.requestFocus();
                 starting_or_clear = true;
             }
+            // If button is a decimal, then add a decimal point in the display field
             else if (buttonOperation.equals(".")) {
                 enterDecimal();
             }
+            // If button is an "=", then perform calculation by invoking the enterEquals() function
             else if (buttonOperation.equals("=")) {
                 enterEquals();
             }
+            // If button is "+", then perform add
             else if (buttonOperation.equals("+")) {
                 add();
             }
+            // If button is "-", then perform subtract
             else if (buttonOperation.equals("-")) {
                 subtract();
             }
+            // If button is "x", then perform multiplication
             else if (buttonOperation.equals("x")) {
                 multiply();
             }
+            // If button is "/", then perform division
             else if (buttonOperation.equals("/")) {
                 divide();
             }
@@ -203,6 +212,9 @@ class CalculatorApp extends JFrame implements ActionListener {
         displayFieldText.requestFocus();
     }
 
+    /**
+     * This method checks for the key presses on keyboard and keypads
+     * */
     public void handleKeyPress(KeyEvent e) {
         int numCode = e.getKeyCode();
 
@@ -262,6 +274,7 @@ class CalculatorApp extends JFrame implements ActionListener {
     public void initNumBtns(JPanel btnPanel) {
         // Initialize numberButtons with 10 JButton objects
         numberButtons = new JButton[12];
+        // For all buttons, updated text and add action listener
         for (int i = 0; i < 10; i++) {
             numberButtons[i] = new JButton(String.valueOf(i));
             numberButtons[i].addActionListener(this);
@@ -283,7 +296,7 @@ class CalculatorApp extends JFrame implements ActionListener {
             button.setBackground(buttonBGNumbers);
             button.setBorder(borderColor);
 
-            // Add mouse listener
+            // Add mouse listener and update the colors
             button.addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseEntered(MouseEvent e) {
@@ -305,6 +318,7 @@ class CalculatorApp extends JFrame implements ActionListener {
         numberButtons[11].setForeground(Color.WHITE);
         numberButtons[11].setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, new Color(77, 167, 196)));
         numberButtons[11].addMouseListener(new MouseAdapter() {
+            // Add mouse listener and update the colors
             @Override
             public void mouseEntered(MouseEvent e) {
                 super.mouseEntered(e);
@@ -323,6 +337,11 @@ class CalculatorApp extends JFrame implements ActionListener {
         btnPanel.add(numberButtons[11]);
     }
 
+    /**
+     * Initliaze the operations buttons (+ - x / CLR)
+     * @params
+     * btnPanel - type JPanel
+     */
     public void initOpBtns(JPanel opPanel) {
         // Initilize the operatorButtons with 4 operators
         operatorButtons = new JButton[4];
@@ -348,7 +367,7 @@ class CalculatorApp extends JFrame implements ActionListener {
             button.setBackground(buttonBGNumbers);
             button.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, new Color(215, 215, 215)));
 
-            // Add mouse listener
+            // Add mouse listener and update colors
             button.addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseEntered(MouseEvent e) {
@@ -369,6 +388,7 @@ class CalculatorApp extends JFrame implements ActionListener {
         clrButton.setBackground(new Color(224, 71, 49));
         clrButton.setForeground(Color.WHITE);
         clrButton.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, new Color(224, 71, 49)));
+        // Add mouse listener and update colors
         clrButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
@@ -382,16 +402,19 @@ class CalculatorApp extends JFrame implements ActionListener {
                 clrButton.setBackground(new Color(224, 71, 49));
             }
         });
-
         // Add buttons to opPanel
         opPanel.add(operatorButtons[0]);
         opPanel.add(operatorButtons[1]);
         opPanel.add(operatorButtons[2]);
         opPanel.add(operatorButtons[3]);
         opPanel.add(clrButton);
-
     }
 
+    /**
+     * This method enters a number to the display of the calculator
+     * @params
+     * int number
+     * */
     public void enterNumber(int number) {
         if (starting_or_clear) {
             displayFieldText.setText("");
@@ -402,6 +425,9 @@ class CalculatorApp extends JFrame implements ActionListener {
         }
     }
 
+    /**
+     * This method enters a decimal in the display of the calculator
+     * */
     public void enterDecimal() {
         boolean decimalExists = decimalExists();
         if (!decimalExists) {
@@ -409,53 +435,68 @@ class CalculatorApp extends JFrame implements ActionListener {
         }
     }
 
+    /**
+     * This method removes a character from the display of the calculator
+     * */
     public void enterBackSpace() {
         String updatedText = backSpaceEntered();
         displayFieldText.setText(updatedText);
     }
 
+    /**
+     * This method detects an ENTER from keyboard or "=" on the calculator button and
+     * performs a calculation
+     * */
     public void enterEquals() {
+        // Track floating numbers num1 and num2
         float num1;
         float num2;
 
-        String previousExpression = calculatedFieldText.getText();
-
-        char firstChar = calculatedFieldText.getText().charAt(0);
-        if (firstChar == '-') {
-            previousExpression = previousExpression.substring(1);
-        }
-
+        // If there is a requested operation (+ - / x):
         if (requestedOperation) {
-            // Update Input Field to show '='
+            // Track the previous expression from the calculated field (top display)
+            String previousExpression = calculatedFieldText.getText();
+            // Check if the first character in the top display has a negative sign
+            char firstChar = calculatedFieldText.getText().charAt(0);
+            if (firstChar == '-') {
+                // Remove the '-' from the display. We have captured the negative sign in firstChar
+                previousExpression = previousExpression.substring(1);
+            }
+            // Update Top Field to show '='
             calculatedFieldText.setText(previousExpression + displayFieldText.getText() + " = ");
             // Remove any non-numeric characters to extract numbers only
             String numericExpression = calculatedFieldText.getText().replaceAll("[^0-9.]", " ");
             String elements[] = numericExpression.split("\\s+");
 
+            // If the first number was a negative, appropriately update the num1
             if (firstChar == '-') {
                 num1 = -Float.parseFloat(elements[0]);
             } else {
                 num1 = Float.parseFloat(elements[0]);
             }
 
-            if (elements.length > 1) {
-                num2 = Float.parseFloat(elements[1]);
-            } else {
-                num2 = ans;
-            }
+            num2 = Float.parseFloat(elements[1]);
             // Update Input Field to show '='
             calculatedFieldText.setText(String.valueOf(num1) + " " + operationEnvoked + " " + String.valueOf(num2) + " = ");
 
-        } else {
-            String numericPreviousExpression = previousExpression.replaceAll("[^0-9.]", " ");
-            String elements[] = numericPreviousExpression.split("\\s+");
-
+        }
+        // If no operation was requested and an ENTER or "=" was detected,
+        // then we simply perform calculation of the result and the num2 with the appropriate previous operation
+        // For example if 3-2 = 1, then we pressed enter, then we would have 1-2 = -1. Where result was 1 and num2 was 2
+        // and the previous operation was a subtraction
+        else {
+            // Update num1 and num2
             num1 = Float.parseFloat(displayFieldText.getText());
             num2 = pnum2;
-
+            // Update the calculated field text (Top Display) of the calculator
             calculatedFieldText.setText(String.valueOf(num1) + " " + prevoperationEnvoked + " " + String.valueOf(num2) + " = ");
         }
 
+        // The below statements check which operation was detected
+        // It updates the previous pnum1 and pnum2 with the current num1 and num2 respectively
+        // Performs a calculation
+        // Updated the previous operation envoked with the current operation envoked
+        // Resets the requestedOperation to false
         if (operationEnvoked == '+') {
             pnum1 = num1;
             pnum2 = num2;
@@ -488,32 +529,33 @@ class CalculatorApp extends JFrame implements ActionListener {
             operationEnvoked = ' ';
             requestedOperation = false;
         }
+
+        // If no operation was envoked and an ENTER or "=" was detected
+        // We proceed to performing the operation previously envoked
         else {
 
             if (!requestedOperation && prevoperationEnvoked == '+') {
-                System.out.println("+ PrevOp Envoked, num1 = " + num1 + ", num2 = " + num2);
                 ans = num1 + num2;
             } else if (!requestedOperation && prevoperationEnvoked == '-') {
-                System.out.println("- PrevOp Envoked, num1 = " + num1 + ", num2 = " + num2);
                 ans = num1 - num2;
             } else if (!requestedOperation && prevoperationEnvoked == 'x') {
-                System.out.println("x PrevOp Envoked, num1 = " + num1 + ", num2 = " + num2);
                 ans = num1 * num2;
-                System.out.println("ans = " + ans);
             } else if (!requestedOperation && prevoperationEnvoked == '/') {
-                System.out.println("/ PrevOp Envoked, num1 = " + num1 + ", num2 = " + num2);
                 ans = num1 / num2;
             } else {
-                System.out.println("No Operation Envoked ");
                 calculatedFieldText.setText("");
                 ans = num1;
             }
         }
-
-        //ans = ans + Float.parseFloat(displayFieldText.getText());
+        // Update the display with the final answer
         displayFieldText.setText(String.valueOf(ans));
     }
 
+    /**
+     * This method performs an add operation
+     * It only works when there is no previous-requested operation
+     * Sets the display fields appropriately, requestedOperation to true and operationEnvoked
+     * */
     public void add() {
         if (!requestedOperation) {
             // Update Input Field to show '+'
@@ -523,9 +565,13 @@ class CalculatorApp extends JFrame implements ActionListener {
             operationEnvoked = '+';
             requestedOperation = true;
         }
-
     }
 
+    /**
+     * This method performs a subtraction operation
+     * It only works when there is no previous-requested operation
+     * Sets the display fields appropriately, requestedOperation to true and operationEnvoked
+     * */
     public void subtract() {
         if (!requestedOperation) {
             // Update Input Field to show '-'
@@ -537,6 +583,11 @@ class CalculatorApp extends JFrame implements ActionListener {
         }
     }
 
+    /**
+     * This method performs a multiplication operation
+     * It only works when there is no previous-requested operation
+     * Sets the display fields appropriately, requestedOperation to true and operationEnvoked
+     * */
     public void multiply() {
         if (!requestedOperation) {
             // Update Input Field to show 'x'
@@ -548,6 +599,11 @@ class CalculatorApp extends JFrame implements ActionListener {
         }
     }
 
+    /**
+     * This method performs a division operation
+     * It only works when there is no previous-requested operation
+     * Sets the display fields appropriately, requestedOperation to true and operationEnvoked
+     * */
     public void divide() {
         if (!requestedOperation) {
             // Update Input Field to show '/'
